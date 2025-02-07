5a5da3605ba815dc0260272858f8e6abeec212ca
Ashish <agaude@netflixcontractors.com>
2025-02-07 22:46:17 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let clients = {};
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        const data = JSON.parse(message);
+
+        switch (data.type) {
+            case "register":
+                clients[data.name] = ws;
+                break;
+            case "call":
+                if (clients[data.target]) {
+                    clients[data.target].send(JSON.stringify({ type: "offer", offer: data.offer, caller: data.caller }));
+                }
+                break;
+            case "answer":
+                if (clients[data.target]) {
+                    clients[data.target].send(JSON.stringify({ type: "answer", answer: data.answer }));
+                }
+                break;
+            case "candidate":
+                if (clients[data.target]) {
+                    clients[data.target].send(JSON.stringify({ type: "candidate", candidate: data.candidate }));
+                }
+                break;
+        }
+    });
+
+    ws.on("close", () => {
+        Object.keys(clients).forEach((name) => {
+            if (clients[name] === ws) delete clients[name];
+        });
+    });
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
